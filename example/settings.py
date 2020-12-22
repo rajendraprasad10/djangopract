07301e2253d001fe08ee1db110b4afb38c44f0fa
rajendra thaidala <rajendra.taidala10@gmail.com>
2020-12-22 06:59:16 +0530

    mysql database has been configered

--- example/settings.py
+++ example/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employeedb',
+        'USER': 'root',
+        'PASSWORD' : 'Rajendra',
+
     }
 }
 
